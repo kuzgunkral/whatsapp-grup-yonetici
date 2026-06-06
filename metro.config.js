d901e72529d09f160f2632c27762ae64b8a6395f
Reis <reis@github.com>
2026-06-06 21:43:51 +0300

    fix: clean metro.config, remove crypto polyfill

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- */
-const config = {
-  resolver: {
-    // Node.js modüllerini React Native'de çalıştırmak için
-    extraNodeModules: {
-      crypto: require.resolve('react-native-crypto'),
-      stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer'),
-      events: require.resolve('events'),
-      process: require.resolve('process/browser'),
-    },
-  },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
