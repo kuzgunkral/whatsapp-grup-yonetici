eb7c63617237d9e1389f67bb84bbdfe5354997ca
Reis <reis@github.com>
2026-06-07 13:07:24 +0300

    fix: add Dockerfile to server for Railway deploy

--- build-apk.ps1
+++
@@ -1,6 +0,0 @@
-$env:JAVA_HOME = "C:\Program Files\Microsoft\jdk-17.0.19.10-hotspot"
-$env:ANDROID_HOME = "$env:LOCALAPPDATA\Android\Sdk"
-$env:PATH = "C:\Program Files\Git\cmd;$env:JAVA_HOME\bin;$env:ANDROID_HOME\platform-tools;$env:PATH"
-
-Set-Location "$PSScriptRoot\android"
-& .\gradlew.bat assembleDebug
